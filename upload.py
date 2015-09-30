402268d334eb335d89a656ad9745a02c87ddc39d
Patrick Kimber <code@pkimber.net>
2015-09-30 19:55:31 +0100

    upload a document (tidy)

--- upload.py
+++ upload.py
@@ -1,11 +1,29 @@
+"""
+
+I created a site called ``scratchpad``.
+
+The URL of this site is:
+http://alfresco.kbsoftware.co.uk:8080/share/page/site/scratchpad/dashboard
+
+"""
+
 import json
 import requests
 
-#url = "http://localhost:8080/alfresco/service/api/upload"
+
 url = "http://alfresco.kbsoftware.co.uk:8080/alfresco/service/api/upload"
-auth = ("patrick.kimber", "cognito")
-files = {"filedata": open("test1.doc", "rb")}
-data = {"siteid": "scratchpad", "containerid": "documentLibrary"}
+auth = (
+    "patrick.kimber",
+    "cognito"
+)
+files = {
+    "filedata": open("test2.doc", "rb")
+}
+data = {
+    "siteid": "scratchpad",
+    "containerid": "documentLibrary",
+}
+
 r = requests.post(url, files=files, data=data, auth=auth)
 print(r.status_code)
 print(json.loads(r.text))
